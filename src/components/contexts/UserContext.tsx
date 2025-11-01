a53791a1469ebe544bee72add3ba90d151c2d9e3
abc100 <abccba111307@gmail.com>
2025-11-01 20:33:38 +0900

    기능 추가
    -크롭핑 완료 후 확인 누르면 크롭핑 된 이미지로 프로필 변경
    -UserContext 구조 개선
    -사진 저장 할 때 사각형으로 저장한 후 렌더링 할 땐 border-radius로 둥글게 보이도록 변경

--- src/components/contexts/UserContext.tsx
+++ src/components/contexts/UserContext.tsx
@@ -1,16 +1,18 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import type { ReactNode } from "react";
 import type { UserContextType, UserInfo } from "../../types/User";
 
-export const UserContext = createContext<UserContextType>({ user: {name: "ZXO", img:"https://cdn.discordapp.com/attachments/1137618300638072832/1419603738586382356/image.png?ex=68fbe3c2&is=68fa9242&hm=e1fde4255067175861ccfd5dff784d9f8fe667f0badca4b0ab9a369f1c93217a&"}, setUser: () => {} });
+export const UserContext = createContext<UserContextType>({ user: {name: "ZXO", img:""}, setUser: () => {} });
 
 /*{ children } 구조분해 할당 받고 { children: ReactNode } 구조분해 할당 받은 children이 ReactNode라고 명시 {} 로 둘다 감싼 건 둘다 객체임을 명시*/
 export const Provider = ({ children }: { children: ReactNode }) => { 
-  const {user, setUser} = useUser();
+  const [user, setUser] = useState<UserInfo>({ name: "ZXO", img: "" });
+
+  const updateUser = (newData: UserInfo) => setUser(newData);
 
   return(
     <>
-      <UserContext value={{user, setUser}}>{children}</UserContext>
+      <UserContext.Provider value={{user, setUser: updateUser}}>{children}</UserContext.Provider>
     </>
   )
 }
@@ -19,9 +21,12 @@ export const useUser = () => {
   const {user, setUser} = useContext(UserContext);
 
   /*나중에 유지보수 1순위  | style쪽 0순위*/
+  /*
+  나중에 필요시 함수 추가
   const editUser = (newData: UserInfo) => {
     setUser(newData);
-  }
+  } 
+  */
 
-  return {user, setUser, editUser}
+  return {user, setUser}
 }
