4ff310615780b72f93a42cc4f3d209888f6d8f1a
abc100 <abccba111307@gmail.com>
2025-11-03 22:11:09 +0900

    기능 추가 [WIP]
    -챗봇이 생각하는 동안 스피너 보이도록 변경
    -챗봇이 채팅을 다다다 치는 것 같은 애니메이션 추가
    -챗봇이 생각, 말하는 동안 채팅 칠 수 없도록 변경
    -채봇이, 생각, 말하는 동안 보내기 버튼 다른 이미지로 수정 해야 될 것 같음(디자인이랑 상의 필요)

--- src/api/chatBot.ts
+++ src/api/chatBot.ts
@@ -1,5 +1,5 @@
-export const postMessage = async (userId: number, content: string) => {
-  const res = await fetch("http://192.168.1.49:8000/Bookshelf_AI", {
+export const postMessage = async (userId: number, content: string, onChunk?: (text: string) => void) => {
+  const res = await fetch("http://192.168.1.15:8000/Bookshelf_AI", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ user_id: userId, user_said: content }),
@@ -14,8 +14,10 @@ export const postMessage = async (userId: number, content: string) => {
   while (true) {
     const { done, value } = await reader.read(); //실시간으로 chunk 받아오기
     if (done) break; //chunk 다 받아오면 done이 true 되면서 while문 탈출
-    fullText += decoder.decode(value, { stream: true });
+    const chunk = decoder.decode(value, { stream: true });
+    fullText += chunk
     /*chunk를 문자열로 해석하지만 { stream: true } 으로 이 데이터가 끝이 아닐 수도 있으니까 다음 chunk까지 기다려 문자열이 깨지지 않도록 함*/
+    if (onChunk) onChunk(chunk);
   }
 
   return fullText;
