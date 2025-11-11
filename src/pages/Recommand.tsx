4a0dd6f999bafd34738221c4aec05b3cc2a521a2
abc100 <abccba111307@gmail.com>
2025-11-11 17:13:47 +0900

    기능 추가
    -스피너 추가
    -책 hover시 ppointer로 뜨도록 변경
    -checked 이미지 변경

--- src/pages/Recommand.tsx
+++ src/pages/Recommand.tsx
@@ -5,15 +5,13 @@ import styled from "styled-components";
 import { postRecommand } from "../api/recommand";
 import { useUser } from "../components/contexts/UserContext";
 import { bouncy } from 'ldrs'
-import recommandResult from "../assets/searchResult.png"
+import Loading from "../components/loading/loading";
 bouncy.register();
 
 export default function RecommandList() {
   const { user } = useUser();
   const [recommandBooks, setRecommandBooks] = useState<RecommandBook[]>([]);
   const [loading, setloading] = useState(true);
-
-  let bookList = [];
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +20,6 @@ export default function RecommandList() {
         const res = await postRecommand(user.name);
         setRecommandBooks(res.data);
         setloading(false);
-        bookList = res.data;
       } catch(error) {
         console.error(error);
       } 
@@ -47,20 +44,7 @@ export default function RecommandList() {
           ></Recoomand>
         ))}
       </Container>
-      {loading && <LoadingContainer >
-        <l-bouncy
-          size="70"
-          speed="2.0" 
-          color="#00C471" 
-        ></l-bouncy>
-      </LoadingContainer>}
-
-      {bookList.length === 0 && !loading &&
-        <ResultContainer>
-          <img src={recommandResult} style={{margin: "155px 0 54px"}} />
-          <Title style={{margin: "0 0 100px 0", color: "#878787"}}>대여한 책이 없습니다</Title>
-        </ResultContainer>
-      }
+      <Loading loading={loading} bookList={recommandBooks} />
     </>
   )
 }
@@ -71,26 +55,4 @@ const Container = styled.div`
   align-items: center;
   margin-top: 75px;
   gap: 35px;
-`
-
-const LoadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 400px;
-`
-
-const Title = styled.h1`
-  margin: 0;
-  font-size: 36px;
-  font-weight: 600;
-  color: black;
-  margin: 0 0 80px 240px;
-`
-
-const ResultContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `
